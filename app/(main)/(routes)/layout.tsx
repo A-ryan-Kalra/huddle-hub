c33fe115536b9fcb3bc03221c3d13f98a8100732
Aryan Kalra <aryan.smart832@gmail.com>
2025-03-29 14:44:01 +0530

    working on layout

--- app/(main)/(routes)/layout.tsx
+++ app/(main)/(routes)/layout.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { currentProfile } from "@/lib/currentProfile";
+
 import React, { useRef, useState } from "react";
 
 interface RoutesLayoutProps {
@@ -16,13 +16,17 @@ function RoutesLayout({ children }: RoutesLayoutProps) {
 
     const startX = e.clientX;
     const startWidth = width;
-
+    console.log(width);
+    let number = 0;
     const handleMove = (event: MouseEvent) => {
       if (!isResizing.current) {
         return null;
       }
-      const newWidth = startWidth + (event.clientX - startX);
-      setWidth(Math.max(newWidth, 150));
+      const newWidth =
+        startWidth +
+        (event.clientX - startX) * ((number * 0.9) / (number * 0.5));
+      number++;
+      setWidth(Math.min(Math.max(newWidth, 150), 2500));
     };
 
     const handleMouseUp = () => {
@@ -37,14 +41,16 @@ function RoutesLayout({ children }: RoutesLayoutProps) {
 
   return (
     <div className="w-full h-full flex">
-      <div style={{ width }} className="relative h-full bg-slate-100">
-        Resizable Content Resizable Content
-        <div
-          onMouseDown={handleDrag}
-          className="absolute h-full border-r-zinc-400 w-[5px] opacity-0 hover:opacity-100 duration-500 transition  cursor-ew-resize hover:border-r-indigo-400 hover:border-r-[3px] top-0 right-0"
-        ></div>
+      <div className="min-w-[70px] h-full "></div>
+      <div className="flex w-full h-full pr-1 pb-2 rounded-2xl  overflow-hidden">
+        <div style={{ width }} className="relative  h-full bg-slate-100">
+          <div
+            onMouseDown={handleDrag}
+            className="absolute h-full border-r-zinc-400 w-[5px] opacity-0 hover:opacity-100 duration-500 transition  cursor-ew-resize hover:border-r-indigo-400 hover:border-r-[3px] top-0 right-0"
+          ></div>
+        </div>
+        <div className="bg-slate-200 w-full h-full">{children}</div>
       </div>
-      <div>{children}</div>
     </div>
   );
 }
---
+++ app/(main)/layout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function MainLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="w-full flex flex-col h-full">
+      <div className="w-full h-[30px]"></div>
+      <div className="w-full h-full relative ">{children}</div>
+    </div>
+  );
+}
+
+export default MainLayout;
