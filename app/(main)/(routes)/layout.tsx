2e41d0ab96da0f52c246814574f2a79910f8b2aa
Aryan Kalra <aryan.smart832@gmail.com>
2025-03-29 13:28:34 +0530

    create components resize bi directional

--- app/(main)/(routes)/layout.tsx
+++ app/(main)/(routes)/layout.tsx
@@ -1,15 +1,48 @@
+"use client";
 import { currentProfile } from "@/lib/currentProfile";
-import React from "react";
+import React, { useRef, useState } from "react";
 
 interface RoutesLayoutProps {
   children: React.ReactNode;
 }
 
-async function RoutesLayout({ children }: RoutesLayoutProps) {
+function RoutesLayout({ children }: RoutesLayoutProps) {
+  const [width, setWidth] = useState(300);
+  const isResizing = useRef(false);
+
+  const handleDrag = (e: React.MouseEvent) => {
+    e.preventDefault();
+    isResizing.current = true;
+
+    const startX = e.clientX;
+    const startWidth = width;
+
+    const handleMove = (event: MouseEvent) => {
+      if (!isResizing.current) {
+        return null;
+      }
+      const newWidth = startWidth + (event.clientX - startX);
+      setWidth(Math.max(newWidth, 150));
+    };
+
+    const handleMouseUp = () => {
+      isResizing.current = false;
+      document.removeEventListener("mousemove", handleMove);
+      document.removeEventListener("mouseup", handleMouseUp);
+    };
+
+    document.addEventListener("mousemove", handleMove);
+    document.addEventListener("mouseup", handleMouseUp);
+  };
+
   return (
     <div className="w-full h-full flex">
-      <div className=" h-full w-[300px] border-r-zinc-400 border-r-2 cursor-ew-resize hover:border-r-indigo-600 duration-500 transition-all">
-        s
+      <div style={{ width }} className="relative h-full bg-slate-100">
+        Resizable Content Resizable Content
+        <div
+          onMouseDown={handleDrag}
+          className="absolute h-full border-r-zinc-400 w-[5px] opacity-0 hover:opacity-100 duration-500 transition  cursor-ew-resize hover:border-r-indigo-400 hover:border-r-[3px] top-0 right-0"
+        ></div>
       </div>
       <div>{children}</div>
     </div>
