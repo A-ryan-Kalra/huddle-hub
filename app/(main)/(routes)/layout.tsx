0369fb6129d414fc80d4b8472d9b23d7800d38ae
Aryan Kalra <aryan.smart832@gmail.com>
2025-05-19 22:48:38 +0530

    add suspense

--- app/(main)/(routes)/layout.tsx
+++ app/(main)/(routes)/layout.tsx
@@ -1,9 +1,12 @@
-import React from "react";
+import Loading from "@/components/ui/loading";
+import React, { Suspense } from "react";
 
 function RoutesLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="w-full flex flex-col h-full">
-      <div className="w-full h-full flex-1 p-1">{children}</div>
+      <div className="w-full h-full flex-1 p-1">
+        <Suspense fallback={<Loading />}>{children}</Suspense>
+      </div>
     </div>
   );
 }
---
+++ components/ui/loading.tsx
@@ -0,0 +1,27 @@
+import { Loader } from "lucide-react";
+import Image from "next/image";
+import React from "react";
+
+function Loading() {
+  return (
+    <div className="flex flex-col h-screen items-center justify-center bg-gray-100">
+      <div className="scale-img relative animate-pulse w-[350px] h-[300px] ">
+        <Image
+          alt="img"
+          className="object-cover"
+          src={"/icons/maskable-icon.png"}
+          fill
+        />
+      </div>
+      <div className="mx-auto  gap-x-1 flex items-center justify-center">
+        <div className="dots ">
+          <span className="dot dot1 bg-black w-1 h-5"></span>
+          <span className="dot dot2 bg-black w-1 h-5"></span>
+          <span className="dot dot3 bg-black w-1 h-5"></span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Loading;
