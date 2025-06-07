917a7870dcd5264627f95ef09d94f40e9c255e3d
Aryan Kalra <aryan.smart832@gmail.com>
2025-06-07 23:05:10 +0530

    setup hero section

--- components/landing-page/header.tsx
+++ components/landing-page/header.tsx
@@ -1,28 +1,84 @@
 "use client";
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NavbarPage from "./navbar";
 import Link from "next/link";
+import { cn } from "@/lib/utils";
 
 function HeaderPage() {
-  return (
-    <header className="sticky top-0 max-w-[1280px] mx-auto flex p-2  items-center justify-between">
-      <Link href={"/"} className="flex items-center gap-x-1 sm:gap-x-3">
-        <div className="relative sm:w-20 sm:h-20  w-16 h-16">
-          <Image
-            draggable="false"
-            alt="/icons/maskable-icon.png"
-            src={"/icons/maskable-icon.png"}
-            className="object-cover scale-[150%] sm:scale-[155%] aspect-square"
-            fill
-          />
-        </div>
-        <h1 className="font-mono font-semibold text-lg sm:text-2xl">
-          Huddle Hub
-        </h1>
-      </Link>
+  const [isScroll, setIsScroll] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
 
-      <NavbarPage />
+  useEffect(() => {
+    function handleScroll(e: Event) {
+      if (window.scrollY > 0) {
+        setIsScroll(true);
+      } else {
+        setIsScroll(false);
+      }
+    }
+
+    function handleMouseMove(e: Event) {
+      const mouseEvent = e instanceof MouseEvent;
+      if (mouseEvent) {
+        setPosition({ x: e.clientX, y: e.clientY });
+      }
+    }
+
+    document.addEventListener("scroll", handleScroll);
+    // document.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      document.removeEventListener("scroll", handleScroll);
+      //   document.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+  //   console.log(position);
+  return (
+    <header
+      className={cn(
+        "w-full  sticky top-0 transition-all duration-100",
+        isScroll && "border-[1px] bg-slate-50/[0.5]"
+      )}
+    >
+      {/* <div
+        style={{
+          position: "fixed",
+          top: position.y,
+          left: position.x,
+          width: "40px",
+          height: "40px",
+          border: "2px solid #5cbacd",
+          borderRadius: "50%",
+          transform: "translate(-50%, -50%)",
+          pointerEvents: "none",
+          zIndex: 9999,
+          transition: "transform 0.05s ease-out",
+        }}
+      /> */}
+
+      <div
+        className={cn(
+          "max-w-[1280px] mx-auto flex p-2  items-center justify-between"
+        )}
+      >
+        <Link href={"/"} className="flex items-center gap-x-1 sm:gap-x-3">
+          <div className="relative sm:w-20 sm:h-20  w-16 h-16">
+            <Image
+              draggable="false"
+              alt="/icons/maskable-icon.png"
+              src={"/icons/maskable-icon.png"}
+              className="object-cover scale-[150%] sm:scale-[155%] aspect-square"
+              fill
+            />
+          </div>
+          <h1 className="font-mono font-semibold text-lg sm:text-2xl">
+            Huddle Hub
+          </h1>
+        </Link>
+
+        <NavbarPage />
+      </div>
     </header>
   );
 }
---
+++ components/landing-page/hero.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HeroPage() {
+  return (
+    <div className="max-w-[1280px] mx-auto flex p-2  items-center bg-black/1 pt-24">
+      <h1 className="text-3xl font-semibold">hero page</h1>
+    </div>
+  );
+}
+
+export default HeroPage;
