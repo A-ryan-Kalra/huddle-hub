94d1dbcde28587cff60557fa63f8e84914bf84a6
Aryan Kalra <aryan.smart832@gmail.com>
2025-06-08 12:32:34 +0530

    add

--- components/landing-page/hero.tsx
+++ components/landing-page/hero.tsx
@@ -3,7 +3,10 @@ import React from "react";
 function HeroPage() {
   return (
     <div className="max-w-[1280px] mx-auto flex p-2  items-center bg-black/1 pt-24">
-      <h1 className="text-3xl font-semibold">hero page</h1>
+      <h1 className="text-3xl font-semibold">Real-Time.Communicate</h1>
+      <span className="text-3xl bg-gradient-to-tr bg-clip-text font-semibold text-transparent from-green-700  to-blue-900">
+        Elevate
+      </span>
     </div>
   );
 }
