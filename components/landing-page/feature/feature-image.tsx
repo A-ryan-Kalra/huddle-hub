72e56b5a3a72adb2c4b3fb8d9f5080e603316669
Aryan Kalra <aryan.smart832@gmail.com>
2025-06-10 22:59:51 +0530

    working on animated popup images

--- components/landing-page/feature/feature-image.tsx
+++ components/landing-page/feature/feature-image.tsx
@@ -1,20 +1,51 @@
+"use client";
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 function FeatureImagePages() {
-  console.log(window.innerHeight);
+  const imageRef = useRef<HTMLDivElement>(null);
+  const heightRef = useRef<HTMLDivElement>(null);
+  const [scrollProgress, setScrollProgress] = useState(0);
+  useEffect(() => {
+    function handleScroll() {
+      const element = imageRef.current;
+      const heightElement = heightRef.current;
+      const windowHeight = window.innerHeight;
+
+      if (!element || !heightElement) {
+        return;
+      }
+      const rect = element.getBoundingClientRect();
+      const targetPoint = 500;
+      if (rect.top < windowHeight - targetPoint && rect.bottom > 0) {
+        const totalScrollable = rect.height;
+        const scrolled = Math.min(
+          totalScrollable,
+          Math.max(0, windowHeight - targetPoint - rect.top)
+        );
+        heightElement.style.height = `${scrolled}px`;
+      }
+    }
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  //   console.log(scrollProgress);
   return (
-    <div className="flex items-start justify-between w-full h-full">
-      <div
-        // style={{ top: `${window.innerHeight}px]` }}
-        className={`  h-[500px] sticky top-[400px]  w-[500px]`}
-      >
-        <Image
-          alt="/modal.png"
-          className="object-cover"
-          fill
-          src={"/modal.png"}
-        />
+    <div
+      ref={imageRef}
+      className="flex items-start relative justify-between w-full h-full"
+    >
+      <div ref={heightRef} className="absolute top-0  ">
+        <div className={`w-[600px] h-[500px] absolute top-full `}>
+          <img
+            className="object-cover aspect-square w-[600px] h-full"
+            alt="/modal.png"
+            src={"/modal.png"}
+          />
+        </div>
       </div>
       <div>
         <h1></h1>
