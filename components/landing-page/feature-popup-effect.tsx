416eb0ec7545e819ab5986403e8d672057a5e076
Aryan Kalra <aryan.smart832@gmail.com>
2025-06-10 10:21:17 +0530

    changes

--- components/landing-page/feature-popup-effect.tsx
+++ components/landing-page/feature-popup-effect.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+"use client";
+import React, { useEffect, useRef, useState } from "react";
 
 function FeaturePopupEffect() {
-  return <div>FeaturePopupEffect</div>;
+  const [height, setHeight] = useState(0);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (!containerRef.current) return;
+
+      const rect = containerRef.current.getBoundingClientRect();
+      const totalHeight = rect.height;
+      const scrollTop = window.scrollY + window.innerHeight - rect.y;
+      console.log(window.scrollY + window.innerHeight);
+      console.log("=======");
+      console.log(rect.top);
+      const progress = Math.min(Math.max(scrollTop / totalHeight, 0), 2);
+
+      setHeight(progress * 200); // max height you want the line to grow
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <div ref={containerRef} className="max-w-[1280px] h-[200vh] relative  ">
+      <div
+        className="sticky top-52 w-1 bg-black"
+        style={{ height: `${height}px` }}
+      ></div>
+    </div>
+  );
 }
 
 export default FeaturePopupEffect;
