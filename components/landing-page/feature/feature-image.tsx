f3a1d4e192f0996042eaf8e6d2dc02a4bcf5854f
Aryan Kalra <aryan.smart832@gmail.com>
2025-06-11 21:43:52 +0530

    show blue border on each checkpoint

--- components/landing-page/feature/feature-image.tsx
+++ components/landing-page/feature/feature-image.tsx
@@ -19,7 +19,7 @@ function FeatureImagePages({
 }) {
   const imageRef = useRef<HTMLDivElement>(null);
   const heightRef = useRef<HTMLDivElement>(null);
-
+  const [isChecked, setIsChecked] = useState(false);
   useEffect(() => {
     function handleScroll() {
       const element = imageRef.current;
@@ -46,9 +46,11 @@ function FeatureImagePages({
           imgEl?.classList.remove("show");
           imgEl?.classList.add("hide");
         }
+        setIsChecked(true);
       } else {
         imgEl?.classList.remove("show");
         imgEl?.classList.add("hide");
+        setIsChecked(false);
       }
     }
 
@@ -57,7 +59,7 @@ function FeatureImagePages({
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
-  }, []);
+  }, [isChecked]);
 
   return (
     <div
@@ -65,9 +67,17 @@ function FeatureImagePages({
       className="flex items-start relative justify-between overflow-hidde   w-full h-[400px]"
     >
       <>
-        <div className="absolute top-0 rounded-full   bg-white py-3  left-[50%] translate-x-[-50%] w-3 h-3  ">
+        <div
+          className={`absolute top-0 rounded-full bg-white left-[50%] translate-x-[-50%] w-3 h-3   ${
+            isChecked
+              ? "border-indigo-600  border-[3px] px-2 py-2"
+              : "bg-white py-3"
+          }`}
+        >
           <div
-            className={`relative rounded-full border-[2px] bg-white top-[50%]  left-[50%] translate-x-[-50%] translate-y-[-50%]  border-indigo-400  w-3 h-3`}
+            className={`relative rounded-full border-[2px]  top-[50%]  left-[50%] translate-x-[-50%] translate-y-[-50%] ${
+              isChecked ? "bg-indigo-600" : "bg-white"
+            } border-indigo-400  w-3 h-3`}
           ></div>
         </div>
         <div ref={heightRef} className="absolute top-0  ">
