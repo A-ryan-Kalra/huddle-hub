d2b948f61b3dae98cf0bb3198ada1cf29c5f825d
Aryan Kalra <aryan.smart832@gmail.com>
2025-06-11 16:53:38 +0530

    add image ScaleIn or ScaleOut animation

--- components/landing-page/feature/feature-image.tsx
+++ components/landing-page/feature/feature-image.tsx
@@ -8,10 +8,14 @@ function FeatureImagePages({
   icon,
   alt,
   className,
+  description,
+  title,
 }: {
   icon: string;
   alt: string;
   className?: string;
+  title: string;
+  description: string;
 }) {
   const imageRef = useRef<HTMLDivElement>(null);
   const heightRef = useRef<HTMLDivElement>(null);
@@ -27,6 +31,7 @@ function FeatureImagePages({
       }
       const rect = element.getBoundingClientRect();
       const targetPoint = 500;
+
       if (rect.top < windowHeight - targetPoint && rect.bottom > 0) {
         const totalScrollable = rect.height;
         const scrolled = Math.min(
@@ -34,7 +39,7 @@ function FeatureImagePages({
           Math.max(0, windowHeight - targetPoint - rect.top)
         );
 
-        if (scrolled > 10 && scrolled < 390) {
+        if (scrolled > 10 && scrolled < 395) {
           imgEl?.classList.add("show");
           imgEl?.classList.remove("hide");
         } else {
@@ -56,27 +61,42 @@ function FeatureImagePages({
   return (
     <div
       ref={imageRef}
-      className="flex items-start relative justify-between  overflow-hidde bg w-full h-[400px]"
+      className="flex items-start relative justify-between overflow-hidde   w-full h-[400px]"
     >
-      <div className="absolute top-0 rounded-full   bg-white py-3  left-[50%] translate-x-[-50%] w-3 h-3  ">
-        <div
-          className={`relative rounded-full border-[2px] bg-white top-[50%]  left-[50%] translate-x-[-50%] translate-y-[-50%]  border-indigo-400  w-3 h-3`}
-        ></div>
-      </div>
-      <div ref={heightRef} className="absolute top-0  ">
-        <div
-          className={cn(`w-[600px] h-[500px] absolute top-full `, className)}
-        >
-          <img
-            className="object-cover aspect-square w-[600px] h-full"
-            alt={alt}
-            src={icon}
-            data-img-new
-          />
+      <>
+        <div className="absolute top-0 rounded-full   bg-white py-3  left-[50%] translate-x-[-50%] w-3 h-3  ">
+          <div
+            className={`relative rounded-full border-[2px] bg-white top-[50%]  left-[50%] translate-x-[-50%] translate-y-[-50%]  border-indigo-400  w-3 h-3`}
+          ></div>
         </div>
-      </div>
-      <div>
-        <h1></h1>
+        <div ref={heightRef} className="absolute top-0  ">
+          <div
+            className={cn(
+              `sm:w-[600px]  w-[200px] h-[200px] sm:h-[500px] absolute top-full `,
+              className
+            )}
+          >
+            <img
+              className="object-cover aspect-square w-[600px] h-full"
+              alt={alt}
+              src={icon}
+              data-img-new
+            />
+          </div>
+        </div>
+      </>
+      <div className="  w-full flex  justify-between h-full">
+        <div className="flex-1 w-full h-full"></div>
+        <div className="flex-1 flex gap-y-5 flex-col  px-2">
+          <h1 className="mt-2 text-left text-2xl ml-2 sm:ml-10  font-extrabold sm:text-4xl">
+            <span className="from-purple-500 to-blue-500 bg-gradient-to-r text-transparent bg-clip-text">
+              {title}
+            </span>
+          </h1>
+          <p className=" sm:text-xl max-sm:text-sm w-full text-left sm:px-5 ml-5 py-2 px-2 font-sans text-zinc-700">
+            {description}
+          </p>
+        </div>
       </div>
     </div>
   );
--- components/landing-page/feature/feature-popup-effect.tsx
+++ components/landing-page/feature/feature-popup-effect.tsx
@@ -27,6 +27,7 @@ function FeaturePopupEffect() {
           totalScrollable,
           Math.max(100, windowHeight - triggerPoint - rect.top)
         );
+
         line.style.height = `${scrolled}px`;
         imge.style.height = `${scrolled}px`;
       } else {
@@ -43,7 +44,6 @@ function FeaturePopupEffect() {
 
           if (!entry.isIntersecting) {
             document.querySelectorAll("[data-img-new]").forEach((img) => {
-              console.log(img);
               img.classList.toggle("hide");
             });
 
@@ -65,7 +65,7 @@ function FeaturePopupEffect() {
   }, []);
 
   return (
-    <div className="max-w-[1280px] w-full relative mx-auto flex h-full flex-col p-2 items-center pt-[4.5rem]  ">
+    <div className=" w-full relative mx-auto flex h-full flex-col p-2 items-center pt-[4.5rem]  ">
       <div
         ref={imgeRef}
         className="w-1 border-[3px] border-gray-300 border-dashed rounded-xl h-full absolute top-0 mx-auto mt-5 transition"
@@ -85,23 +85,63 @@ function FeaturePopupEffect() {
       </div>
       <div
         ref={lineRef}
-        className="w-1 bg-indigo-700 rounded-xl absolute top-0 mx-auto mt-5 transition"
+        className="w-[5px] bg-indigo-700  rounded-xl absolute top-0 mx-auto mt-5 transition"
         style={{ height: "100px" }}
       ></div>
 
       <div ref={sectionRef} className="h-full w-full bg-gray-">
-        <div className="bg-white w-1 h-1" data-img-to-show-new="modal"></div>
         <FeatureImagePages
+          title="Threaded Chats"
+          description="Threaded chats in Huddle-hub allow users to have focused, organized
+            conversations within a specific message, keeping side discussions
+            out of the main channel flow. By replying in a thread, team members
+            can discuss a topic in depth without cluttering the channel."
           alt="modal"
-          // className="active-slide"
+          // className="show"
           icon="/modal.png"
         />
-        <div className="bg-white w-1 h-1" data-img-to-show-new="env-card"></div>
-        <FeatureImagePages alt="env-card" icon="/env-card.png" />
-        <div className="bg-white w-1 h-1" data-img-to-show-new="deploy"></div>
-        <FeatureImagePages alt="deploy" icon="/type.png" />
-        {/* <div className="bg-white w-1 h-1" data-img-to-show-new="type"></div> */}
-        {/* <FeatureImagePages alt="deploy" icon="/modal.png" /> */}
+        <FeatureImagePages
+          title="Push Notifications"
+          description="Introducing push notifications, It keep users instantly informed, even when they're not actively browsing. With this feature, users can receive real-time updates — like new messages, alerts or important announcements — directly on their device."
+          alt="env-card"
+          icon="/env-card.png"
+        />
+        <FeatureImagePages
+          title="Jump to Message"
+          description="Quickly navigate to the exact point in a conversation with our Jump to Message feature. Whether you're following up on a reply, a mention, or a thread, just tap and instantly scroll to the original message."
+          alt="deploy"
+          icon="/type.png"
+        />
+        <FeatureImagePages
+          title="Notification Tracking"
+          description="Our website includes a notification tracking feature that lets users view and manage all their notifications in one place. Whether it's a new message, update, alert, or reminder — everything is stored and organized so users never miss anything important."
+          alt="deploy"
+          icon="/type.png"
+        />
+        <FeatureImagePages
+          title="Invite Friends"
+          description="Easily grow your network by inviting friends to join the platform. With our Invite Friends feature, users can send personalized invitations via link. It's a simple way to share the experience, collaborate with others."
+          alt="type"
+          icon="/type.png"
+        />
+        <FeatureImagePages
+          title="Live Video Call"
+          description="Connect in real time with seamless live video calls directly through the platform.Ensuring high-quality video, secure connections, and instant communication — all in one place whether for meetings, collaboration, or casual chats."
+          alt="type"
+          icon="/type.png"
+        />
+        <FeatureImagePages
+          title="Create Channels"
+          description="Organize conversations your way with the ability to create private or public channels. Stay in control of who sees what — and keep communication organized and secure."
+          alt="type"
+          icon="/type.png"
+        />
+        <FeatureImagePages
+          title="Create Server"
+          description="Build your own workspace for community, collaboration, or team communication with the Create Server feature. Servers act as hubs where you can organize channels, manage members, set permissions, and customize settings to fit your needs — whether it's for friends, a project team, or a large community."
+          alt="type"
+          icon="/type.png"
+        />
       </div>
     </div>
   );
