72bd33db37956622bfea21441c454a5bb80071c9
Aryan Kalra <aryan.smart832@gmail.com>
2025-06-14 16:42:50 +0530

    fixes

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -6,25 +6,30 @@ import Header from "@/components/landing-page/header/header";
 import HeroPage from "@/components/landing-page/hero/hero";
 import FeaturePages from "@/components/landing-page/feature/feature";
 import Footer from "@/components/landing-page/footer";
+import { currentUser } from "@clerk/nextjs/server";
+import { initialProfile } from "@/lib/initialProfile";
 
 export default async function Home() {
-  const profile = await currentProfile();
+  const user = await currentUser();
+  if (user) {
+    const profile = await initialProfile();
 
-  if (profile) {
-    const server = await db.server.findFirst({
-      where: {
-        members: {
-          some: {
-            profileId: profile?.id,
+    if (profile) {
+      const server = await db.server.findFirst({
+        where: {
+          members: {
+            some: {
+              profileId: profile?.id,
+            },
           },
         },
-      },
-    });
+      });
 
-    if (server) {
-      redirect(`/servers/${server.id}`);
-    } else if (profile && !server) {
-      redirect(`/servers/create-server`);
+      if (server) {
+        redirect(`/servers/${server.id}`);
+      } else if (profile && !server) {
+        redirect(`/servers/create-server`);
+      }
     }
   }
 
