79484578747ca3410f26bb5aee4b0581029343fb
Aryan Kalra <aryan.smart832@gmail.com>
2025-06-17 14:06:22 +0530

    update footer

--- components/landing-page/footer.tsx
+++ components/landing-page/footer.tsx
@@ -7,21 +7,15 @@ function Footer() {
     <footer className={`bg-white`}>
       <div className="flex relative justify-around items-center dark:border-white dark:text-white duration-300 py-5 border-t-2 border-black">
         <span className="md:text-xl text-[14px] flex items-center justify-center gap-x-1">
-          {new Date().getFullYear()} &copy; All Rights Reserved
+          Made with ❤️ for our users. &copy; {new Date().getFullYear()} Huddle
+          hub
           <Link
             target="__blank"
             href={"https://github.com/A-ryan-Kalra/huddle-hub"}
             className="bg-slate-300 rounded-full p-1 hover:bg-slate-200 cursor-pointer transition"
           >
-            <Github className="fill-white" />
+            <Github className="fill-white w-4 sm:w-5 h-4 sm:h-5" />
           </Link>
-        </span>
-        <span className="md:text-xl text-[14px] spacex items-center flex justify-center">
-          Made with{" "}
-          <span className="flex items-center justify-center ml-1  gap-x-1">
-            <Keyboard /> &
-            <Mouse />
-          </span>
         </span>
       </div>
     </footer>
